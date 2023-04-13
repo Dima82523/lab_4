281cef84b7ef20c3b023c1daf64b6ff75b3c5405
Zver <dima.852.dma@gmail.com>
2023-04-13 22:40:10 +0300

    add Note.kt

---
+++ app/src/androidTest/java/ui/components/Note/Note.kt
@@ -0,0 +1,30 @@
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Checkbox
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.topic2.android.notes.theme.rwGreen
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Note(){
+
+    Box(
+        modifier = Modifier
+            .size(40.dp)
+            .background(rwGreen)
+    )
+    Text(text = "Загловок", maxLines = 1)
+    Text(text = "Содержание", maxLines = 1)
+    Checkbox(checked = false, onCheckedChange = {},
+    modifier = Modifier.padding(start = 8.dp))
+}
+
+@Preview
+@Composable
+private fun NotePreview(){ Note()
+}
