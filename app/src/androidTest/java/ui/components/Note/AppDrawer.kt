16544b6d6c2ad7de3b7837d26c3449ea5463a3a0
Zver <dima.852.dma@gmail.com>
2023-04-13 23:08:52 +0300

    change AppDrawer(LightDarkThemeItem)

--- app/src/androidTest/java/ui/components/Note/AppDrawer.kt
+++ app/src/androidTest/java/ui/components/Note/AppDrawer.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
@@ -114,5 +115,38 @@ fun ScreenNavigationButtonPreviev(){
             isSelected = true,
             onClick = { }
         )
+    }
+}
+
+@Composable
+private fun LightDarkThemeItem(){
+    Row(
+        Modifier
+            .padding(8.dp)
+    ){
+        Text(
+            text = "Включить темную тему",
+            style = MaterialTheme.typography.body2,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.6f),
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 8.dp, top = 8.dp, end = 8.dp, bottom = 8.dp)
+                .align(alignment = Alignment.CenterVertically)
+        )
+        Switch(
+            checked = JetNotesThemeSettings.isDarkThemeEnabled,
+            onCheckedChange = { JetNotesThemeSettings.isDarkThemeEnabled = it},
+            modifier = Modifier
+                .padding(start = 8.dp, end = 8.dp)
+                .align(alignment = Alignment.CenterVertically)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun LightDarkThemeItemPreview(){
+    JetNotesTheme {
+        LightDarkThemeItem()
     }
 }
